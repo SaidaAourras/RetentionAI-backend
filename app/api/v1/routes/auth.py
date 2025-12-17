1023df35cc74482adc82385174f630c142d37267
SaidaAourras <sa123aouras@gmail.com>
2025-12-17 23:38:08 +0100

    RPDDAR-89 add create jwt to login endpoint

--- app/api/v1/routes/auth.py
+++ app/api/v1/routes/auth.py
@@ -1,9 +1,9 @@
 from fastapi import APIRouter , Depends  , HTTPException
 from sqlalchemy.orm import Session
-from ..schemas.user import UserCreate , LoginRequest , UserResponse
+from ..schemas.user import UserCreate , LoginRequest , TokenResponse
 from ..dependencies import get_db
 from app.services.user_services import create_user , verify_user_is_exists
-# from app.services.auth_service import verify_token , create_token
+from app.services.auth_services import  create_token
 
 
 auth_router = APIRouter(prefix='/auth' ,tags=["authentication"])
@@ -26,12 +26,21 @@ def register(user:UserCreate , db:Session=Depends(get_db)):
     
 
 
-@auth_router.post('/login' )
+@auth_router.post('/login' , response_model=TokenResponse)
 def login(user:LoginRequest , db:Session = Depends(get_db)):
     user_exists = verify_user_is_exists(user , db)
+    
     if user_exists:
-        user_dict = user.model_dump()
-        return user_dict
+        token_payload = {
+            "sub": str(user_exists.id),
+            "email": user_exists.email
+        }
+        access_token = create_token(token_payload)
+        
+        return TokenResponse(
+            access_token=access_token,
+            token_type="bearer"
+        )
             
     raise HTTPException(
         status_code=401,
@@ -39,6 +48,6 @@ def login(user:LoginRequest , db:Session = Depends(get_db)):
     )
 
 
-# @auth_router.post("/logout")
-# def logout():
-#     return {"message": "OK"}
+@auth_router.post("/logout")
+def logout():
+    return {"message": "OK"}
