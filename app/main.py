d1e0017aa8e131c6aaea6758101d3c5db0ec087e
SaidaAourras <sa123aouras@gmail.com>
2025-12-17 21:48:31 +0100

    initialize main

---
+++ app/main.py
@@ -0,0 +1,11 @@
+from fastapi import FastAPI
+from app.db.database import engine
+from app.db.models.base import Base
+from app.api.v1.routes.auth import auth_hello
+
+app = FastAPI()
+
+
+Base.metadata.create_all(engine)
+
+app.include_router(auth_hello , prefix='/api/v1')
